40c78d15ce626596a39a16d28ce48fc44fe3dd6b
victor <victor_27edge@hotmail.com>
2021-10-05 02:38:45 -0500

    Se agrego codigo en el archivo llamado Lista donde mostramos SOLO LOS NOMBRES en pantalla insertados en la base de datos, este archivo de insercion de base de datos se abre en el admin del servidor en el archivo llamado datos.php

--- listas.js
+++ listas.js
@@ -1,17 +1,53 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import {isTemplateElement} from '@babel/types';
+import React, {Component} from 'react';
+import {View, Text, FlatList, Dimensions} from 'react-native';
 
 export default class listas extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      datos: [],
     };
   }
+
+  componentDidMount = () => {
+    let _this = this;
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        console.log(this.responseText);
+        var temp = JSON.parse(xhttp.responseText);
+        _this.setState({datos: temp});
+      }
+    };
+    xhttp.open(
+      'GET',
+      'https://zekt1209.000webhostapp.com/mostrar.php',
+      true,
+    );
+    xhttp.send();
+  };
 
   render() {
     return (
       <View>
-        <Text> listas </Text>
+        <FlatList style={{marginTop: 10}} data={this.state.datos} 
+        renderItem=
+        {({item}) => (
+          <View style={{justifyContent: 'center', marginBottom: 10}}>
+            <Text
+              style={{
+                backgroundColor: 'blue',
+                color: 'white',
+                padding: 10,
+                width: Dimensions.get('window').width,
+              }}>
+            {item.Nombre} 
+            </Text>
+          </View>
+        )}
+        keyExtractor={item => item.Codigo}
+        />
       </View>
     );
   }
